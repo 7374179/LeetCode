7065abe8c6a22612b4ceb4124cfe36db90bb238f
7374179 <108831857+7374179@users.noreply.github.com>
2023-06-24 00:44:29 -0700

    Time: 0 ms (100.00%), Space: 40.9 MB (47.38%) - LeetHub

---
+++ 1150-check-if-a-number-is-majority-element-in-a-sorted-array/1150-check-if-a-number-is-majority-element-in-a-sorted-array.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isMajorityElement(int[] nums, int target) {
+        Arrays.sort(nums);
+
+        int left=0;
+        int right=nums.length-1;
+        int sum=0;
+
+        while(left<right){
+            boolean flag = false;
+            if(nums[left]<target){
+                left++;
+                flag = true;
+            }
+            if(nums[right]>target){
+                right--;
+                flag = true;
+            }
+            if(!flag){
+                sum=right-left+1;
+                left=right;
+            }
+        }
+        return sum>nums.length/2;
+
+    }
+}
