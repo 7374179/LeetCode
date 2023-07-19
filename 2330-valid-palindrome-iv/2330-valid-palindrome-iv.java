1ce4c74bc04695e6f6d6d1afb7db37314d71dcbd
7374179 <108831857+7374179@users.noreply.github.com>
2023-07-19 00:21:20 -0700

    Time: 2 ms (100.00%), Space: 43.8 MB (16.31%) - LeetHub

---
+++ 2330-valid-palindrome-iv/2330-valid-palindrome-iv.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean makePalindrome(String s) {
+        int left=0;
+        int right=s.length()-1;
+        int count=2;
+
+        while(left<=right){
+            if(s.charAt(left)!=s.charAt(right)){
+                count--;
+                if(count<0){
+                    return false;
+                }
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
