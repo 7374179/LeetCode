1bd7e7459e2087f966cc520909b212e9eca1c856
7374179 <108831857+7374179@users.noreply.github.com>
2023-08-19 17:03:18 -0700

    Time: 2 ms (10.07%), Space: 43.9 MB (5.66%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.java
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -10,14 +10,12 @@
  */
 class Solution {
     public ListNode oddEvenList(ListNode head) {
-        // Stack<Integer> stack = new Stack<>();
         Queue<Integer> que = new LinkedList<>();
         ListNode p1 = head;
         ListNode p2 = p1;
 
         while(head!=null && head.next!=null){
             que.add(head.next.val);
-            // stack.add(head.next.val);
             p1.next=p1.next.next;
             if(p1.next==null){
                 break;
