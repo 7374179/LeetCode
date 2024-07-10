b1e827bd1dfcc31c7f64df43731750326c50ce73
Taehyuk Chung <7374179@gmail.com>
2024-07-09 18:53:31 -0700

    Time: 1 ms (99.42%), Space: 42.3 MB (13.66%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.java
+++ 1598-crawler-log-folder/1598-crawler-log-folder.java
@@ -1,14 +1,14 @@
 class Solution {
     public int minOperations(String[] logs) {
         int main = 0;
-        String parent = "../";
-        String remain = "./";
+        // String parent = "../";
+        // String remain = "./";
         for(int i=0;i<logs.length;i++){
-            if(logs[i].equals(parent)){
+            if(logs[i].equals("../")){
                 if(main>0){
                     main--;
                 }
-            }else if(logs[i].equals(remain)){
+            }else if(logs[i].equals("./")){
                 continue;
             }else{
                 main++;
