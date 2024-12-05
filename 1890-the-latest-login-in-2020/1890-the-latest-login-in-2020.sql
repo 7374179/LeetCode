9be719807cae63a9beb8c475f83411a0908bfc30
Taehyuk Chung <7374179@gmail.com>
2024-12-04 20:25:53 -0800

    Time: 1152 ms (7.18%), Space: 0B (100.00%) - LeetHub

---
+++ 1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+
+select user_id, time_stamp as last_stamp
+from logins
+where (user_id, time_stamp) in (
+    select user_id, max(time_stamp)
+    from logins
+    where year(time_stamp)='2020'
+    group by user_id
+)
